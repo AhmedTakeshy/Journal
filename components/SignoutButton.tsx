fdf45f83dff0e5a8b93495763ad348e961efc110
AhmedTakeshy <Ahmedtakeshy7@gmail.com>
2023-11-02 09:07:02 +0300

    🐞 fix(base url): change to dynamic
    change env process env string to stacks back instead

--- components/SignoutButton.tsx
+++ components/SignoutButton.tsx
@@ -3,6 +3,6 @@ import {signOut} from "next-auth/react"
 import { Button } from "./ui/button"
 export default function SignoutButton() {
   return (
-      <Button className="w-full md:w-auto" onClick={() => signOut({ callbackUrl: "${process.env.BASE_URL}" })} variant="destructive">Sign out</Button>
+      <Button className="w-full md:w-auto" onClick={() => signOut({ callbackUrl: `${process.env.BASE_URL}` })} variant="destructive">Sign out</Button>
   )
 }
